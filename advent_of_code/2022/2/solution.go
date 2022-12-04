0b84bbcb90da57e3b67eb8640721fea1b30c984a
Austin Vazquez <macedonv@amazon.com>
2022-12-03 20:48:09 -0800

    add: December 2, 2022 solution

---
+++ advent_of_code/2022/2/solution.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+// Solution for AoC 2022 Day 2
+// https://adventofcode.com/2022/day/2
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+const (
+	Rock     = 1
+	Paper    = 2
+	Scissors = 3
+
+	ShouldLose = 1
+	ShouldDraw = 2
+	ShouldWin  = 3
+)
+
+type Sign struct {
+	value int
+}
+
+func NewSign(character string) (*Sign, error) {
+	switch character {
+	case "A":
+		return &Sign{Rock}, nil
+	case "B":
+		return &Sign{Paper}, nil
+	case "C":
+		return &Sign{Scissors}, nil
+	case "X":
+		return &Sign{ShouldLose}, nil
+	case "Y":
+		return &Sign{ShouldDraw}, nil
+	case "Z":
+		return &Sign{ShouldWin}, nil
+	default:
+		return nil, fmt.Errorf("invalid sign %s", character)
+	}
+}
+
+func (s *Sign) Value() int {
+	return s.value
+}
+
+func (s *Sign) Beats(other *Sign) bool {
+	switch s.value {
+	case Rock:
+		return other.value == Scissors
+	case Paper:
+		return other.value == Rock
+	case Scissors:
+		return other.value == Paper
+	default:
+		panic("Invalid sign")
+	}
+}
+
+func (s *Sign) WinningMove() int {
+	switch s.value {
+	case Rock:
+		return Paper
+	case Paper:
+		return Scissors
+	case Scissors:
+		return Rock
+	default:
+		panic("Invalid sign")
+	}
+}
+
+func (s *Sign) LosingMove() int {
+	switch s.value {
+	case Rock:
+		return Scissors
+	case Paper:
+		return Rock
+	case Scissors:
+		return Paper
+	default:
+		panic("Invalid sign")
+	}
+}
+
+type Round struct {
+	expected  *Sign
+	suggested *Sign
+}
+
+func (r *Round) ScoreFromSuggestedMove() int {
+	extra := 0
+	if r.expected.Value() == r.suggested.Value() {
+		extra = 3
+	} else if r.suggested.Beats(r.expected) {
+		extra = 6
+	}
+	return r.suggested.Value() + extra
+}
+
+func (r *Round) ScoreFromSuggestedOutcome() int {
+	if r.suggested.Value() == ShouldDraw {
+		return 3 + r.expected.Value()
+	} else if r.suggested.Value() == ShouldWin {
+		return 6 + r.expected.WinningMove()
+	}
+	return r.expected.LosingMove()
+}
+
+func ReadRounds(r io.Reader) ([]Round, error) {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanWords)
+
+	var rounds []Round
+	for scanner.Scan() {
+		expected, err := NewSign(scanner.Text())
+		if err != nil {
+			return rounds, err
+		}
+		scanner.Scan()
+		suggested, err := NewSign(scanner.Text())
+		if err != nil {
+			return rounds, err
+		}
+		rounds = append(rounds, Round{expected, suggested})
+	}
+
+	return rounds, nil
+}
+
+func Map(array []Round, f func(Round) int) []int {
+	ints := make([]int, len(array))
+	for _, n := range array {
+		ints = append(ints, f(n))
+	}
+	return ints
+}
+
+func Reduce(array []int, f func([]int) int) int {
+	return f(array)
+}
+
+func main() {
+	handle, err := os.Open("input.txt")
+	check(err)
+	defer handle.Close()
+
+	rounds, err := ReadRounds(handle)
+	check(err)
+
+	scoreFromSuggestedMove := func(r Round) int {
+		return r.ScoreFromSuggestedMove()
+	}
+
+	scoreFromSuggestedOutcome := func(r Round) int {
+		return r.ScoreFromSuggestedOutcome()
+	}
+
+	sum := func(scores []int) int {
+		total := 0
+		for _, score := range scores {
+			total += score
+		}
+		return total
+	}
+
+	fmt.Printf("Part 1: %d\n", Reduce(Map(rounds, scoreFromSuggestedMove), sum))
+	fmt.Printf("Part 2: %d\n", Reduce(Map(rounds, scoreFromSuggestedOutcome), sum))
+}
