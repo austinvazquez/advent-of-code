36ced452700d5f101407d245928912fc073f89c5
Austin Vazquez <macedonv@amazon.com>
2023-12-02 20:50:29 -0800

    add: python solution for December 2, 2023
    Signed-off-by: Austin Vazquez <macedonv@amazon.com>

---
+++ advent_of_code/2023/2/solution.py
@@ -0,0 +1,53 @@
+"""
+Solution for AoC 2023 Day 2
+https://adventofcode.com/2023/day/2
+"""
+from collections import Counter
+from functools import reduce
+from math import prod
+
+
+def main():
+    with open("input.txt") as file:
+        games = [line for line in file]
+
+    print(f"Step 1: {sum(map(is_game_possible, games))}")
+    print(f"Step 2: {sum(map(minimum_power_of_game, games))}")
+
+
+def is_game_possible(game: str) -> int:
+    game = game.lstrip("Game ")
+
+    prefix, suffix = game.split(":")
+
+    game_id = int(prefix)
+    cube_limits = {
+        "red": 12,
+        "blue": 14,
+        "green": 13,
+    }
+
+    for round in suffix.split(";"):
+        for color in round.split(","):
+            count, color = color.strip().split(" ")
+            if int(count) > cube_limits[color]:
+                return 0
+
+    return game_id
+
+
+def minimum_power_of_game(game: str) -> int:
+    _, game = game.split(":")
+
+    min_count = {"red": 0, "blue": 0, "green": 0}
+
+    for round in game.split(";"):
+        for color in round.split(","):
+            count, color = color.strip().split(" ")
+            min_count[color] = max(min_count[color], int(count))
+
+    return prod(min_count.values())
+
+
+if __name__ == "__main__":
+    main()
---
+++ advent_of_code/2023/2/test_solution.py
@@ -0,0 +1,30 @@
+import pytest
+import solution
+
+
+@pytest.mark.parametrize(
+    "game,expected",
+    [
+        ("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", 1),
+        ("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", 2),
+        ("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", 0),
+        ("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", 0),
+        ("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 5),
+    ],
+)
+def test_is_game_possible(game, expected):
+    assert solution.is_game_possible(game) == expected
+
+
+@pytest.mark.parametrize(
+    "game,expected",
+    [
+        ("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", 48),
+        ("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", 12),
+        ("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", 1560),
+        ("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", 630),
+        ("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 36),
+    ],
+)
+def test_minimum_power_of_game(game, expected):
+    assert solution.minimum_power_of_game(game) == expected
